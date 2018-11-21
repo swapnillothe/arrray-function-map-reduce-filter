c83baf0f7818fc74cf880657400a7d8769cbb635
swapnillothe <44019530+swapnillothe@users.noreply.github.com>
2018-11-21 17:15:16 +0530

    Declare functions in scope where it needed;

--- test/libTest.js
+++ test/libTest.js
@@ -1,33 +1,21 @@
 const assert = require('assert');
 const { map, filter } = require('../src/lib.js');
 
-const isEven = function(number){
-  return (number%2)==0;
-}
-
-const double = function(number){
-  return number*2;
-}
-
-const square = function(number){
-  return number*number;
-}
-
-const cube = function(number){
-  return number*square(number);
-}
-
 describe('testing map function',function(){
   let array1,array2,array3;
+  let double = x=>x*2;
+  let square = x=>x*x;
+  let cube = x=>x*x*x;
+
   beforeEach(function(){
     array1 = [1, 2, 3];
     array2 = [3];
     array3 = [-1 , 0, 3];
   });
   it('should return array with double all elements',function(){
-    assert.deepEqual(map(double,array1),[2,4,6]);
-    assert.deepEqual(map(double,array2),[6]);
-    assert.deepEqual(map(double,array3),[-2, 0, 6]);
+    assert.deepEqual(map(double,array1),[ 2, 4, 6 ]);
+    assert.deepEqual(map(double,array2),[ 6 ]);
+    assert.deepEqual(map(double,array3),[ -2, 0, 6 ]);
   });
   it('should return with square all elements',function(){
     assert.deepEqual(map(square,array1),[1,4,9]);
@@ -42,6 +30,7 @@ describe('testing map function',function(){
 });
 
 describe('testing filter function' ,function(){
+  let isEven = x=>(x%2==0); 
   it('should return array with only even numbers ',function(){
     assert.deepEqual(filter(isEven,[1,2,3,4,5,6]),[2,4,6]);
     assert.deepEqual(filter(isEven,[1,3,6]),[6]);
