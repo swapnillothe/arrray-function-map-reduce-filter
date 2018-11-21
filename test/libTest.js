d5a3b4d5b9b131a481b53b3e4bbcbbee04083e42
swapnillothe <44019530+swapnillothe@users.noreply.github.com>
2018-11-21 17:42:16 +0530

    Correct message for test.

--- test/libTest.js
+++ test/libTest.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const { map, filter } = require('../src/lib.js');
 
-describe('testing map function',function(){
+describe('map',function(){
   let array1,array2,array3;
   let double = x=>x*2;
   let square = x=>x*x;
@@ -12,28 +12,29 @@ describe('testing map function',function(){
     array2 = [3];
     array3 = [-1 , 0, 3];
   });
+
   it('should return array with double all elements',function(){
     assert.deepEqual(map(double,array1),[ 2, 4, 6 ]);
     assert.deepEqual(map(double,array2),[ 6 ]);
     assert.deepEqual(map(double,array3),[ -2, 0, 6 ]);
   });
   it('should return with square all elements',function(){
-    assert.deepEqual(map(square,array1),[1,4,9]);
-    assert.deepEqual(map(square,array2),[9]);
-    assert.deepEqual(map(square,array3),[1, 0, 9]);
+    assert.deepEqual(map(square,array1),[ 1, 4, 9 ]);
+    assert.deepEqual(map(square,array2),[ 9 ]);
+    assert.deepEqual(map(square,array3),[ 1, 0, 9 ]);
   });
   it('should return with cube all elements',function(){
-    assert.deepEqual(map(cube,array1),[1,8,27]);
-    assert.deepEqual(map(cube,array2),[27]);
-    assert.deepEqual(map(cube,array3),[-1, 0, 27]);
+    assert.deepEqual(map(cube,array1),[ 1, 8, 27 ]);
+    assert.deepEqual(map(cube,array2),[ 27 ]);
+    assert.deepEqual(map(cube,array3),[ -1, 0, 27 ]);
   });
 });
 
-describe('testing filter function' ,function(){
+describe('filter' ,function(){
   let isEven = x=>(x%2==0); 
   it('should return array with only even numbers ',function(){
-    assert.deepEqual(filter(isEven,[1,2,3,4,5,6]),[2,4,6]);
-    assert.deepEqual(filter(isEven,[1,3,6]),[6]);
+    assert.deepEqual(filter(isEven,[ 1, 2, 3, 4, 5, 6 ]),[ 2, 4, 6 ]);
+    assert.deepEqual(filter(isEven,[ 1, 3, 6 ]),[ 6 ]);
     assert.deepEqual(filter(isEven,[]),[]);
   });
 });
