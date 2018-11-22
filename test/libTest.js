73680c03f574742f28066dee6d2846851bbb115c
swapnillothe <44019530+swapnillothe@users.noreply.github.com>
2018-11-22 07:21:34 +0530

    Add one more test case.

--- test/libTest.js
+++ test/libTest.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const { map, filter } = require('../src/lib.js');
 
-describe('map',function(){
+describe('testing map function',function(){
   let array1,array2,array3;
   let double = x=>x*2;
   let square = x=>x*x;
@@ -11,22 +11,33 @@ describe('map',function(){
     array1 = [1, 2, 3];
     array2 = [3];
     array3 = [-1 , 0, 3];
+    emptyArray = [];
   });
 
-  it('should return array with double all elements',function(){
-    assert.deepEqual(map(double,array1),[ 2, 4, 6 ]);
-    assert.deepEqual(map(double,array2),[ 6 ]);
-    assert.deepEqual(map(double,array3),[ -2, 0, 6 ]);
+  describe('map' ,function(){
+    it('should return array with double all elements',function(){
+      assert.deepEqual(map(double,array1),[ 2, 4, 6 ]);
+      assert.deepEqual(map(double,array2),[ 6 ]);
+      assert.deepEqual(map(double,array3),[ -2, 0, 6 ]);
+    });
+    it('should return with square all elements',function(){
+      assert.deepEqual(map(square,array1),[ 1, 4, 9 ]);
+      assert.deepEqual(map(square,array2),[ 9 ]);
+      assert.deepEqual(map(square,array3),[ 1, 0, 9 ]);
+    });
+    it('should return with cube all elements',function(){
+      assert.deepEqual(map(cube,array1),[ 1, 8, 27 ]);
+      assert.deepEqual(map(cube,array2),[ 27 ]);
+      assert.deepEqual(map(cube,array3),[ -1, 0, 27 ]);
+    });
   });
-  it('should return with square all elements',function(){
-    assert.deepEqual(map(square,array1),[ 1, 4, 9 ]);
-    assert.deepEqual(map(square,array2),[ 9 ]);
-    assert.deepEqual(map(square,array3),[ 1, 0, 9 ]);
-  });
-  it('should return with cube all elements',function(){
-    assert.deepEqual(map(cube,array1),[ 1, 8, 27 ]);
-    assert.deepEqual(map(cube,array2),[ 27 ]);
-    assert.deepEqual(map(cube,array3),[ -1, 0, 27 ]);
+
+  describe('map' ,function(){
+    it('of empty array should return empty array.',function(){
+      assert.deepEqual(map(double,emptyArray),[]);
+      assert.deepEqual(map(square,emptyArray),[]);
+      assert.deepEqual(map(cube,emptyArray),[]);
+    });
   });
 });
 
