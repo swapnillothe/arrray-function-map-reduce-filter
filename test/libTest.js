3973558b2d9e42094be21becdabac0c660a4865a
swapnillothe <44019530+swapnillothe@users.noreply.github.com>
2018-11-22 07:38:24 +0530

    Remove unnecessary test cases for test.

--- test/libTest.js
+++ test/libTest.js
@@ -2,35 +2,12 @@ const assert = require('assert');
 const { map, filter } = require('../src/lib.js');
 
 describe('testing map function',function(){
-  let array1,array2,array3;
   let double = x=>x*2;
-  let square = x=>x*x;
-  let cube = x=>x*x*x;
 
   beforeEach(function(){
-    array1 = [1, 2, 3];
-    array2 = [3];
-    array3 = [-1 , 0, 3];
     emptyArray = [];
     singleElement = [ 2 ];
-  });
-
-  describe('map' ,function(){
-    it('should return array with double all elements',function(){
-      assert.deepEqual(map(double,array1),[ 2, 4, 6 ]);
-      assert.deepEqual(map(double,array2),[ 6 ]);
-      assert.deepEqual(map(double,array3),[ -2, 0, 6 ]);
-    });
-    it('should return with square all elements',function(){
-      assert.deepEqual(map(square,array1),[ 1, 4, 9 ]);
-      assert.deepEqual(map(square,array2),[ 9 ]);
-      assert.deepEqual(map(square,array3),[ 1, 0, 9 ]);
-    });
-    it('should return with cube all elements',function(){
-      assert.deepEqual(map(cube,array1),[ 1, 8, 27 ]);
-      assert.deepEqual(map(cube,array2),[ 27 ]);
-      assert.deepEqual(map(cube,array3),[ -1, 0, 27 ]);
-    });
+    multipleElement = [ 1, 2, 3, 4, 5 ];
   });
 
   describe('map' ,function(){
@@ -45,7 +22,7 @@ describe('testing map function',function(){
   });
   describe('map' ,function(){
     it('should return array with same arity and mapping elements',function(){
-      assert.deepEqual( map( double, singleElement ), [ 4 ] );
+      assert.deepEqual( map( double, multipleElement ), [ 2, 4, 6, 8, 10 ] );
     });
   });
 });
