0284f036df6cadcc679ddc7876153ff7d5e01b60
swapnillothe <44019530+swapnillothe@users.noreply.github.com>
2018-11-23 09:43:23 +0530

    Remove unnecessary describe block.

--- test/libTest.js
+++ test/libTest.js
@@ -1,45 +1,30 @@
 const assert = require('assert');
 const { map, filter } = require('../src/lib.js');
 
-describe('testing map function',function(){
+describe('map',function(){
   let double = x=>x*2;
 
-  describe('map' ,function(){
-    it('of empty array should return empty array.',function(){
-      let emptyArray = [];
-      assert.deepEqual( map( double, emptyArray ), [] );
-    });
+  it('of empty array should return empty array.',function(){
+    let emptyArray = [];
+    assert.deepEqual( map( double, emptyArray ), [] );
   });
 
-  describe('map' ,function(){
-    it('of single element should return array with that mapping element',function(){
-      let singleElement = [ 2 ];
-      assert.deepEqual( map( double, singleElement ), [ 4 ] );
-    });
+  it('of single element should return array with that mapping element',function(){
+    let singleElement = [ 2 ];
+    assert.deepEqual( map( double, singleElement ), [ 4 ] );
   });
 
-  describe('map' ,function(){
-    it('should return array with same arity and mapping elements',function(){
-      let multipleElement = [ 1, 2, 3, 4, 5 ];
-      assert.deepEqual( map( double, multipleElement ), [ 2, 4, 6, 8, 10 ] );
-    });
+  it('should return array with same arity and mapped elements',function(){
+    let multipleElement = [ 1, 2, 3, 4, 5 ];
+    assert.deepEqual( map( double, multipleElement ), [ 2, 4, 6, 8, 10 ] );
   });
 });
 
-describe('testing filter function',function(){
-  let isEven = x=>(x%2==0); 
-  describe('filter' ,function(){
-    it('should return array with only even numbers ',function(){
-      assert.deepEqual( filter( isEven, [ 1, 2, 3, 4, 5, 6 ] ),[ 2, 4, 6 ] );
-      assert.deepEqual( filter( isEven, [ 1, 3, 6 ] ),[ 6 ] );
-      assert.deepEqual( filter( isEven, []),[] );
-    });
-  });
+describe('filter' ,function(){
+  let isEven = x=>(x%2==0);
 
-  describe('filter' ,function(){
-    it('should return empty array for filter empty array',function(){
-      let emptyArray = [];
-    });
+  it('should return empty array for filtering empty array',function(){
+    let emptyArray = [];
+    assert.deepEqual( filter( isEven, emptyArray), [] );  
   });
 });
-
